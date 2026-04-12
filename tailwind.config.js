225b8148886589548086c8dc73d8762995346981
Ujwal Gowda V <ujwalgowda2002v@gmail.com>
2026-04-12 22:51:03 +0530

    Font changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,37 @@ export default {
         modified: "#2b8389",
       },
       fontFamily: {
-        display: 'Oswald, ui-serif',
-      }
+        sans: ["Outfit", "sans-serif"],
+      },
+      fontSize: {
+        xs: ["12px", { lineHeight: "16px" }],
+        sm: ["14px", { lineHeight: "20px" }],
+        base: ["16px", { lineHeight: "24px" }],
+        lg: ["18px", { lineHeight: "28px" }],
+        xl: ["20px", { lineHeight: "28px" }],
+        "2xl": ["24px", { lineHeight: "32px" }],
+        "3xl": ["30px", { lineHeight: "36px" }],
+        "4xl": ["36px", { lineHeight: "44px" }],
+      },
+
+      fontWeight: {
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+      },
+
+      spacing: {
+        18: "4.5rem",
+        22: "5.5rem",
+        26: "6.5rem",
+      },
+
+      letterSpacing: {
+        tight: "-0.01em",
+        normal: "0",
+        wide: "0.02em",
+      },
     },
   },
   plugins: [],
